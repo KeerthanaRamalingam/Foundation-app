9af4928b54f68db342e1f30edb7dcf2e59f4c2ed
Keerthana Ramalingam <keerthu.ram28@gmail.com>
2021-06-30 19:16:29 +0530

    Add contracts and deploy scripts

---
+++ hardhat.config.js
@@ -0,0 +1,37 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const ALCHEMY_API_KEY = "MpewRPt7EhGwnJ3LjRHLK115W1oX5ScA";
+const ROPSTEN_PRIVATE_KEY = "8d5049db7490378850d402931264bcd26691b5ab902de138216df5f56dae4c8e";
+
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: { enabled: process.env.DEBUG ? false : true },
+    }
+  },
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
+
+};
---
+++ scripts/deployNFT.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile 
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const FNDNFT721 = await hre.ethers.getContractFactory("FNDNFT721");
+  const FNDNFT721_ = await FNDNFT721.deploy();
+
+  await FNDNFT721_.deployed();
+
+  console.log("Greeter deployed to:", FNDNFT721_.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
